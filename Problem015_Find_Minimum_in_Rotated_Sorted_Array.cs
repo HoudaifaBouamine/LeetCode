9b786c9ede84ef0d30e740751888ee412d729181
houdaifa bouamine <houdaifa.bouamine@gmail.com>
2023-10-02 13:19:16 +0100

    Solving Binary search problem (Find Minimum in Rotated Sorted Array) , But performence are bad

---
+++ Problem015_Find_Minimum_in_Rotated_Sorted_Array.cs
@@ -0,0 +1,36 @@
+    public class Solution
+    {
+
+        public int binary_search(int[] nums,int min,int max)
+        {
+
+            if (min > max) return int.MaxValue;
+
+            int mid = (max + min+1) / 2;
+            int target = int.MaxValue;
+
+            if (mid == 0)
+            {
+                target = nums[mid];
+            }
+            else if (nums[mid] < nums[mid - 1])
+            {
+                target = nums[mid];
+            }
+            else
+            {
+                target = Math.Min(binary_search(nums, mid+1, max),binary_search(nums,min,mid-1));
+            }
+
+            return target;
+            
+        }
+        public int FindMin(int[] nums)
+        {
+
+
+            return binary_search(nums,0,nums.Length-1);
+
+
+        }
+    }
